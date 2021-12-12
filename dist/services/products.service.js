febd6f0e3ab17e607bbd1d8465d62abd2e74e56d
hiiamt34 <d17dccn195@gmail.com>
2021-12-12 22:57:44 +0700

    sevice v1

--- dist/services/products.service.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ProductService = void 0;
-const service_1 = require("./service");
-class ProductService extends service_1.BaseService {
-}
-exports.ProductService = ProductService;
-//# sourceMappingURL=products.service.js.map
